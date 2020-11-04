5a690feb9bef5386763a95e1c9f8358e8e464261
Robin Tail <robin_tail@me.com>
2020-11-04 18:23:18 +0100

    Next levels: the slam.

--- lib/level.ts
+++ lib/level.ts
@@ -19,6 +19,26 @@ export const levels: Record<SetName, FigureName[]> = {
   G: ['L', 'V', 'P', 'Z', 'Y', 'W', 'N', 'F']
 };
 
+/*
+the slam:
+    {
+      A: ['L', 'P', 'U', 'F', 'W', 'Y', 'Z', 'Y', 'V'],
+      B: ['Y', 'N', 'P', 'U', 'Z', 'V', 'T', 'L', 'F'],
+      C: ['L', 'Y', 'V', 'T', 'W', 'Z', 'F', 'U', 'P'],
+      D: ['N', 'V', 'P', 'U', 'F', 'W', 'L', 'Z', 'T'],
+      E: ['L', 'Y', 'N', 'P', 'W', 'T', 'V', 'F', 'U'],
+      F: ['L', 'P', 'U', 'Z', 'T', 'F', 'N', 'W', 'Y'],
+      G: ['L', 'V', 'P', 'U', 'W', 'N', 'Y', 'T', 'Z'],
+      H: ['L', 'N', 'P', 'T', 'W', 'Z', 'Y', 'F', 'V'],
+      I: ['L', 'Y', 'V', 'U', 'Z', 'F', 'W', 'N', 'T'],
+      J: ['L', 'V', 'P', 'F', 'W', 'Y', 'U', 'T', 'N'],
+      K: ['L', 'Y', 'N', 'U', 'W', 'F', 'P', 'V', 'Z'],
+      L: ['Y', 'V', 'P', 'Z', 'W', 'N', 'F', 'T', 'U'],
+      M: ['L', 'Y', 'V', 'P', 'F', 'U', 'T', 'Z', 'W'],
+      N: ['L', 'Y', 'P', 'Z', 'W', 'N', 'U', 'V', 'T']
+    }
+ */
+
 export function loadLevel(boardSize: number, setName: SetName): Level {
   const board = new Board(boardSize);
   const figures = createFigures();
